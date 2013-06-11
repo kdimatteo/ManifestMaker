55f9e4a335fe24b8f3c75d1c9e530cdfd883278f
Keith Dimatteo <keith@hauthaus.net>
2013-06-11 16:09:44 -0400

    adding build version and java.properties grep

--- promote.sh
+++ promote.sh
@@ -1,79 +1,92 @@
 #!/bin/bash
+#
+# @file promote_static.sh
+# @author ktdimatteo@constultantemail.com
+# @description Deploy to stage-static
+# 
+# This is the script executed by Jenkins/Hudson to move files from accurev (depo:ux, stream:wmc-bootstrap, dir:publish-static) 
+# to the deploy dir. Jenkins then triggers "Deploy CDN" in Nolio to move these file to stage-static.wellmanage.com
+#
+# You probably dont want to modify this unless the hosting environment or deploy process has changed 
+#
+
+
+# get the build version from the ANT properties file
+export BUILD_VERSION=`sed '/^[[:space:]]*\#/d' config/project.properties | grep build.version | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+
+
 
 #Prompt the user to select a target promotion environment
-if [ $# -lt 1 ]
-then echo Usage: promote.sh [Version]
-exit;
+if [[ $1 == "stage" ]]; then
+	export DOMAIN=http://stage-static.wellmanage.com/bootstrap/$BUILD_VERSION
+elif [[ "$1" == "prod" ]]; then
+	export DOMAIN=http://static.wellmanage.com/bootstrap/$BUILD_VERSION
+else
+	echo "Bogus env, please use stage or prod"
+	exit;
 fi
 
 # config
 export DIR_DEPLOY=/deploy/capps/cdn/bootstrap
-export DIR_SRC=/build/capps/cdn/bootstrap/publish-static
-export MANIFEST=manifest.txt
-export LOG=log.txt
+
+#export DIR_SRC=/build/capps/cdn/bootstrap/publish-static
+export DIR_SRC=../publish-static
+
+# path prefixes to move files from and to
+export NOLIO_SRC=/deploy/capps/cdn/bootstrap/publish-static
+export NOLIO_DEPLOY=/apps/cdn/bootstrap
+
+# file handling
+#pwd: /tech/tools/hudson-ws/jobs/Bootstrap build/workspace/build
+export MANIFEST=../../manifest.txt
 
 #pull from accurev
-/apps/wmc/scripts/updReftree bootstrap_build_reftree
+#/apps/wmc/scripts/updReftree bootstrap_build_reftree
 
 # for debugging, you can simply display the dir listings
 #find $FILES_SRC -type f -exec echo '{}' \;
 
 # we will store file lists in these arrays
 FILES_SRC=()
-FILES_DEPLOY=()
 
 # loop through the src dir and stip non-matching path prefixes
 # or use this -exec to kerp them : for file in `find $DIR_SRC -type f -exec echo '{}' \;`
 for file in `find $DIR_SRC -type f -exec sh -c 'echo {} | sed s:$DIR_SRC::g' \;`
 do
-  FILES_SRC+=($file)
+	FILES_SRC+=($file)
 done
 
-# loop through the log and generate an array of previous deploy files
-while read line
-do
-	FILES_DEPLOY+=($line)
-done < $LOG
-
 # notify user of current status
-echo SRC ${#FILES_SRC[@]} items in $DIR_SRC
-echo LOG ${#FILES_DEPLOY[@]} items in $LOG
-
-# our array of both files sorted by dictionary order
-COMBINED=(`for R in "${FILES_SRC[@]}" "${FILES_DEPLOY[@]}" ; do echo "$R" ; done | sort -d`)
+echo SRC: ${#FILES_SRC[@]} items in $DIR_SRC
 
 # delete the manifest if it exists
 if [ -f $MANIFEST ]; then
 	rm $MANIFEST
 fi
+touch $MANIFEST
 
-# write a formatted manifest file
-COLCOUNT=0
-for file in ${COMBINED[@]}; do
-	printf $file >> $MANIFEST
-	if [[ $(( $COLCOUNT % 2 )) == 0 ]]; then
-		printf "\t" >> $MANIFEST
-	elif [[ $(( $COLCOUNT % 2 )) == 1 ]]; then
-		printf "\n" >> $MANIFEST
+
+for file in ${FILES_SRC[@]}
+do
+	RESPONSE=$(curl --write-out %{http_code} --silent --output /dev/null $DOMAIN$file)
+	if [ "$RESPONSE" == "404" ]; then
+		echo "To Promote: $NOLIO_DEPLOY$file"
+		echo
+		printf $NOLIO_SRC$file" "$NOLIO_DEPLOY$file"\n" >> $MANIFEST
 	fi
-	COLCOUNT=$((COLCOUNT+1))
 done
-
-#update the logfile
-if [ -f $LOG ]; then
-	rm $LOG
-fi
-
-for line in ${FILES_SRC[@]}; do
-	echo $line >> $LOG
-done
-
 
 #Copy the war file into the deployment directory
 rm -rf $DIR_DEPLOY
 mkdir -p $DIR_DEPLOY
-cp -r $DIR_SRC/ $DIR_DEPLOY
-cp $DIR_SRC/../build/deploy.xml $DIR_DEPLOY/
+cp -r $DIR_SRC/../build/deploy.xml $DIR_DEPLOY
+
+mkdir -p $NOLIO_SRC
+cp -r $DIR_SRC/../publish-static/* $NOLIO_SRC
+
+cp $MANIFEST $DIR_DEPLOY/
+echo Copied $MANIFEST to $DIR_DEPLOY/
+
 
 istatus=$?
 if [ $istatus != 0 ]; then
@@ -82,3 +95,7 @@ if [ $istatus != 0 ]; then
 	echo "Reason: Unable to Copy"
 	exit;
 fi
+
+echo "-----------------------"
+echo "Promote Static Complete"
+echo "-----------------------"
